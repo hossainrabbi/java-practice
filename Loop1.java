67a93918c176a20011b817b6286085482e370883
Hossain Rabbi <hossainrabbi259@gmail.com>
2021-08-11 19:40:30 +0600

    added first series number: 1 + 2 + 3 + ... + n

--- Loop1.java
+++ Loop1.java
@@ -1,1 +1,21 @@
 // 1 + 2 + 3 + 4 + .... + n
+
+import java.util.Scanner;
+
+public class Loop1{
+    public static void main(String[] args) {
+        int n, sum = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Series number: ");
+        n = input.nextInt();
+        input.close();
+
+        for(int i = 1; i <= n; i++){
+            sum += i;
+        }
+
+        System.out.println("Total number: " + sum);
+    }
+}
